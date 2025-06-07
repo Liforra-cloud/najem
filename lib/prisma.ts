c680b899a6098327187318c92f22881f8f924092
Liforra-cloud <info.liforra@gmail.com>
2025-06-07 16:33:29 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,20 @@
 // lib/prisma.ts
-
 import { PrismaClient } from '@prisma/client';
 
-// Zabráníme vícenásobné inicializaci v režimu hot-reload
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
-  global.prisma ||
+  global.prisma ??
   new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
+    log: ['warn', 'error'],
+    datasources: {
+      db: {
+        url: process.env.DIRECT_URL || process.env.DATABASE_URL, // 👈 tohle zajistí použití přímé URL
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
